0a5814cf7ebe6348bf0f9798362433e58b7b32cf
Kyle Bolton <hello@kylebolton.me>
2020-10-15 18:50:49 +0000

    User Card

---
+++ src/Components/UserCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const UserCard = (loginName, avatarUrl, followers) => {
+  return <Card>UserCard</Card>;
+};
+
+export default UserCard;
+
+const Card = styled.div`
+  background: #fff;
+  padding: 20px;
+`;
